8557d3053fbcf4ccc52b813175f9931ee7ed08ff
Asmei <kamsykula@gmail.com>
2020-05-16 19:01:22 +0200

    test 2 refactor

--- LoadBalancerKata/LoadBalancerTests/src/LoadBalancer/ServerLoadBalancerTest.cpp
+++ LoadBalancerKata/LoadBalancerTests/src/LoadBalancer/ServerLoadBalancerTest.cpp
@@ -10,20 +10,6 @@
 void ServerLoadBalancerTest::SetUp()
 {
 }
-VmSPtr ServerLoadBalancerTest::a(const VmBuilder& builder)
-{
-   return builder.build();
-}
-
-VmBuilder ServerLoadBalancerTest::vm() const
-{
-   return {};
-}
-
-ServerSPtr ServerLoadBalancerTest::a(const ServerBuilder& builder)
-{
-   return builder.build();
-}
 
 std::vector<ServerSPtr> ServerLoadBalancerTest::aListOfServersWith(const ServerSPtr& server)
 {
@@ -57,7 +43,7 @@ TEST_F(ServerLoadBalancerTest, balancingAServer_noVms_serverStaysEmpty)
 TEST_F(ServerLoadBalancerTest, balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm)
 {
    ServerSPtr theServer = a(ServerBuilder::server().withCapacity(1));
-   VmSPtr theVm = a(vm().ofSize(1));
+   VmSPtr theVm = a(VmBuilder::vm().ofSize(1));
    balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
    EXPECT_THAT(*theServer, hasLoadPercentageOf(100.0));
