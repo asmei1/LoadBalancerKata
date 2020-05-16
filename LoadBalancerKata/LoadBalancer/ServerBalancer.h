a677dc904bf894bb263914f0b498b06ca7dae9e4
Asmei <kamsykula@gmail.com>
2020-05-17 00:14:44 +0200

    test 6 refactor

--- LoadBalancerKata/LoadBalancer/ServerBalancer.h
+++ LoadBalancerKata/LoadBalancer/ServerBalancer.h
@@ -8,7 +8,11 @@
 class ServerBalancer
 {
 public:
-   ServerSPtr extractLessLoadServer(const std::vector<ServerSPtr>& servers);
    void balance(const std::vector<ServerSPtr>& servers, const std::vector<VmSPtr>& vms);
+
+private:
+   void addToCapableLessLoadedServer(const std::vector<ServerSPtr>& servers, VmSPtr vm);
+   ServerSPtr extractLessLoadServer(const std::vector<ServerSPtr>& servers);
+   std::vector<ServerSPtr> serversThatCanFitVm(const std::vector<ServerSPtr>& servers, VmSPtr vm);
 };
 
